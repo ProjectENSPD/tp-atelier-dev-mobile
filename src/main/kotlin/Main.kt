84293abc7de8bba7cf55181c4a98e46c697f9574
Jordy <jordykom18@gmail.com>
2023-09-29 19:05:01 +0100

    Work done

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,24 @@
-fun main(args: Array<String>) {
-    println("Hello World! ${args[0]}")
+fun main() {
+    println("Hello Mr Hyoubissie!")
+//question 1
+    var dirtyLevel1 = 20
+    val waterFilter1 = { dirty : Int -> dirty / 2}
+    println(waterFilter1(dirtyLevel1))
+
+//question 2
+    val waterFilter2: (Int) -> Int = { dirty -> dirty / 2 }
+    println(updateDirty(30, waterFilter2))
+
+//question 3
+    println(updateDirty(15, ::increaseDirty))
+
+    var dirtyLevel2 = 19
+    dirtyLevel2 = updateDirty(dirtyLevel2) { dirtyLevel -> dirtyLevel + 23}
+    println(dirtyLevel2)
 }
+fun updateDirty(dirty: Int, operation: (Int) -> Int): Int {
+    return operation(dirty)
+}
+
+fun increaseDirty( start: Int ) = start + 1
+
